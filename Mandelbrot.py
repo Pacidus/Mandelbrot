b43671e3857075abea37f62fce8fb416bae5a434
Pacidus <yohan.duarte@laposte.net>
2017-05-13 22:53:17 +0200

    Update Mandelbrot.py

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,29 +1,40 @@
-#!/usr/bin/env Python3
+#!/usr/bin/python3.5
+# -*- coding: utf-8 -*-
+
 from PIL import Image,ImageDraw
-from random import randrange
-from time import time
-    
+from math import sqrt
 
 def mandelbrot(x,y):
+    """int*int -> Nonetype
+    Hypothèse: x > 0 and y > 0
+    retourne la fractale de mandelbrot"""
     a = 0
     b = 0
     img = Image.new("RGB",(x, y), "#FFFFFF")
     draw = ImageDraw.Draw(img)
     for i in range(y):
         for j in range(x):
-            Ci = ((i*2.4)/y)-1.2
-            Cr = ((j*2.7)/x)-2.1
-            Zi = 0
-            Zr = 0
-            for n in range(355):
-                ZR = Zr
-                Zr = Zr**2 - Zi**2 + Cr
-                Zi = 2 * ZR * Zi + Ci
-                if Zr**2 + Zi**2 >= 4:
-                    break
-            if Zr**2 + Zi**2 >= 4:
-                draw.point((j,i),fill=(0,0,n))
-            else:
-                draw.point((j,i),fill=(0,0,0))
+            draw.point((j,i),fill=(0,0,point(x,y,i,j)))
     img.show()
     img.save("/home/mahtar_nola/Bureau/Programation/test.png", "PNG")
+
+def point( x, y, i, j, Xmax = 1, Xmin = -2, Ymax = 1, Ymin = -1):
+    """int^4 * float^4 -> int
+    Hypoyhèse: min([x,y,i,j]) >= 0 and Xmin < Xmax and Ymin < Ymax
+    retourne le nombre d'itération avant que la suite Zn diverge vers l'infini"""
+    Ci = ((i*(Ymax-Ymin))/y)+Ymin
+    Cr = ((j*(Xmax-Xmin))/x)+Xmin
+    Zi = 0
+    Zr = 0
+    if ((Cr+1)**2)+(Ci**2) < 1/16:
+        return(0)
+    p = sqrt(((Cr-(1/4))**2)+(Ci**2))
+    if Cr < p-(2*(p**2))+(1/4):
+        return(0)
+    for n in range(256):
+        ZR = Zr
+        Zr = Zr**2 - Zi**2 + Cr
+        Zi = 2 * ZR * Zi + Ci
+        if Zr**2 + Zi**2 >= 4:
+            return(n)
+    return(0)
